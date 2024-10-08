27ad5ee8636ecec8ca1f72020783e96d5a3fbb51
seunggonhyeon <hyunsg5638@gmail.com>
2024-10-08 20:25:49 +0900

    first commit

---
+++ Sample.java
@@ -0,0 +1,21 @@
+class Animal {
+    String name; // 객체 변수
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        Animal cat = new Animal();
+        cat.setName("boby");
+
+        Animal dog = new Animal();
+        dog.setName("happy");
+
+        System.out.println(cat.name);
+        System.out.println(dog.name);
+
+    }
+}
